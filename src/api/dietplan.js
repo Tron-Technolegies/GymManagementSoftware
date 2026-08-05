1defbebea4649a7eac341d9484a438e0b1471ad2
savithnarayan111 <savithnarayan111@gmail.com>
2026-08-05 15:50:42 +0530

    Add AI diet generation

--- src/api/dietplan.js
+++ src/api/dietplan.js
@@ -1,5 +1,10 @@
 import api from "./api";
 
 export const generateDietPlan = (memberId) => {
-    return api.post("admin/api/generate_diet/", { member_id: memberId, });
+    return api.post(
+        "admin/api/generate-diet/",
+        {
+            member_id: memberId,
+        }
+    );
 };
